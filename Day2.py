be21e189b30c1b2ccf19a6c7e1ac464c013e89a4
Aaron Spencer Leake <leakea@purdue.edu>
2020-12-02 00:14:59 -0500

    Day 2 complete

--- Day2.py
+++ Day2.py
@@ -2,20 +2,34 @@
 puzzles from Day 2."""
 
 from common.imports import importAdventFile
-from common.perm_functions import multiplicativeSum
-from itertools import permutations
+import re
 
 data = importAdventFile('data/Day2Input')
-data = [int(string) for string in data]
+data = [string.split() for string in data]
 
 def FirstPart():
-    """ Find the two numbers that add to 2020 and return their product"""
-    return multiplicativeSum(data,2020,2)
+    """ Find the passwords that have the correct number of specified letters """
+    output = 0
+    for n,letter,password in data:
+        actual = password.count(letter[0])
+        amounts = n.split('-')
+        if int(amounts[0]) <= actual <= int(amounts[1]):
+            output += 1
+
+    
+    return output
 
 
 def SecondPart():
-    """ Find the three numbers that add to 2020 and return their product"""
-    return multiplicativeSum(data,2020,3)
+    """ Find the passwords that have the correct number of specified letters in the specified 
+        locations"""
+    output = 0
+    for indices,letter,password in data:
+        positions = indices.split('-')
+        first = password[int(positions[0])-1] == letter[0]
+        second = password[int(positions[1])-1] == letter[0]
+        output += 1 if first+second == 1 else 0
+    return output
 
 
 print("First Puzzle Answer:\t{}\nSecond Puzzle Answer:\t{}\n".format(FirstPart(),SecondPart()))
