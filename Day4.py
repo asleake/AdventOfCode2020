85eb7a29439209164586e5d9abd46a165694c5ab
Aaron Spencer Leake <leakea@purdue.edu>
2020-12-04 11:22:10 -0500

    Day 4 finished

--- Day4.py
+++ Day4.py
@@ -2,15 +2,54 @@
 puzzles from Day 4."""
 
 from common.imports import importAdventFile
+from common.passport import Passport
 data = importAdventFile('data/Day4Input')
 
 def FirstPart():
     """ Find ... """ 
-    return 1
+    passportIndex = 0
+    unenteredPassports = [[]]
+    for line in data:
+        if line == '':
+            passportIndex +=1
+            unenteredPassports.append([])
+            continue
+        for datum in line.split(' '):
+            unenteredPassports[passportIndex].append(datum.split(':'))
+
+    validPassports = 0
+    passports = []
+    for unenteredPassport in unenteredPassports:
+        passport = Passport(unenteredPassport)
+        passports.append(passport)
+        if passport.checkForDataFields():
+            validPassports += 1
+            
+    return validPassports
 
 
 def SecondPart():
     """ Find ... """ 
-    return 2
+    passportIndex = 0
+    unenteredPassports = [[]]
+    for line in data:
+        if line == '':
+            passportIndex +=1
+            unenteredPassports.append([])
+            continue
+        for datum in line.split(' '):
+            unenteredPassports[passportIndex].append(datum.split(':'))
+
+    validPassports = 0
+    passports = []
+    for unenteredPassport in unenteredPassports:
+        passport = Passport(unenteredPassport)
+        passports.append(passport)
+        if passport.checkForDataFields():
+            if passport.checkForDataValidity():
+                validPassports += 1
+            
+    return validPassports
+
 
 print("First Puzzle Answer:\t{}\nSecond Puzzle Answer:\t{}\n".format(FirstPart(),SecondPart()))
