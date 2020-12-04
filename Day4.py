bbc4e3c4eca9b116b89b2446c19274551c258a9a
Aaron Spencer Leake <leakea@purdue.edu>
2020-12-03 23:36:27 -0500

    Day 4 set-up

---
+++ Day4.py
@@ -0,0 +1,16 @@
+"""Day 4 of Advent of Code 2020. Running this file will print out the correct answers to the two
+puzzles from Day 4."""
+
+from common.imports import importAdventFile
+data = importAdventFile('data/Day4Input')
+
+def FirstPart():
+    """ Find ... """ 
+    return 1
+
+
+def SecondPart():
+    """ Find ... """ 
+    return 2
+
+print("First Puzzle Answer:\t{}\nSecond Puzzle Answer:\t{}\n".format(FirstPart(),SecondPart()))
