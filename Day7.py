35b4430b0b3969c4c40b652b72999ad2c50b531a
Aaron Spencer Leake <leakea@purdue.edu>
2020-12-06 20:27:13 -0500

    Day 7 prepped

---
+++ Day7.py
@@ -0,0 +1,18 @@
+"""Day 7 of Advent of Code 2020. Running this file will print out the correct answers to the two
+puzzles from Day 7."""
+
+from common.imports import importAdventFile
+
+data = importAdventFile('data/Day7Input')
+
+def FirstPart():
+    """ Find ... """
+    return 1
+
+
+def SecondPart():
+    """ Find ... """
+    return 2
+
+
+print("First Puzzle Answer:\t{}\nSecond Puzzle Answer:\t{}\n".format(FirstPart(),SecondPart()))
