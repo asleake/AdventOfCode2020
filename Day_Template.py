c5a51585d9b4bede70208a8c76f4acb8aa88376f
Aaron Spencer Leake <leakea@purdue.edu>
2020-12-07 01:35:28 -0500

    Added a new template to be used for each day

---
+++ Day_Template.py
@@ -0,0 +1,19 @@
+"""Day _ of Advent of Code 2020. Running this file will print out the correct answers to the two
+puzzles from Day _."""
+
+from common.imports import importAdventFile, bagSplit
+
+data = importAdventFile('data/Day_Input')
+
+
+def FirstPart():
+    """ Find ... """
+    return 1
+    
+
+def SecondPart():
+    """ Find ... """
+    return 2
+
+
+print("First Puzzle Answer:\t{}\nSecond Puzzle Answer:\t{}\n".format(FirstPart(),SecondPart()))
