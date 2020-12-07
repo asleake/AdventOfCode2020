621b798d2eccbde7615693b8f95e5f0ccc28280c
Aaron Spencer Leake <leakea@purdue.edu>
2020-12-07 01:36:14 -0500

    Added blank day 8 files

---
+++ Day8.py
@@ -0,0 +1,19 @@
+"""Day 8 of Advent of Code 2020. Running this file will print out the correct answers to the two
+puzzles from Day 8."""
+
+from common.imports import importAdventFile, bagSplit
+
+data = importAdventFile('data/Day8Input')
+
+
+def FirstPart():
+    """ Find ... """
+    return 1
+    
+
+def SecondPart():
+    """ Find ... """
+    return 2
+
+
+print("First Puzzle Answer:\t{}\nSecond Puzzle Answer:\t{}\n".format(FirstPart(),SecondPart()))
